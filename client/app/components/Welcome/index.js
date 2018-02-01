c24247e48407e81ac1f96c2f4375ef04b7619184
77Vincent <wentianqi77@outlook.com>
2018-02-01 22:46:16 +0800

    1. remove old src directory 2. add CSS normalizer

--- client/app/components/Welcome/index.js
+++ client/app/components/Welcome/index.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'React';
 import fetch from 'isomorphic-fetch';
+
 export default class Welcome extends  Component{
   constructor(props){
     super(props);
   }
   componentDidMount(){
-    fetch('/api/majors').then(res=>res.json()).then((res)=> console.log(res) );
   }
   render(){
     return (
       <div>
-        This is welcome page11111
-      </div>);
+        <header></header>
+      </div>
+    );
   }
 }
