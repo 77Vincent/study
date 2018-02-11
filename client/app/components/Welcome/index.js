110aac812869c8043306c6116c6c533402f7c80b
Wen <vwen@china.pcs.local>
2018-02-11 11:42:04 +0800

    welcome page enhacnement

--- client/app/components/Welcome/index.js
+++ client/app/components/Welcome/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'antd';
+import { Button, Row, Col } from 'antd';
 import './index.less';
 
 export default class Welcome extends React.Component {
@@ -8,41 +8,34 @@ export default class Welcome extends React.Component {
     super(props);
   }
   render() {
-    // Temp variables
-    const placeholder = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`;
-    const promote = ['Why Here', 'Testimonial', 'Contact Us'];
-
     return (
       <div className='Welcome'>
-        <div className='Welcome-intro'>
-          <div className='App-background-image'></div>
+        <Row className='Welcome-intro' type='flex' align='middle'>
+          <div className='App-background-image' />
 
-          <div className='Welcome-slogan'>
-            <h1>
-                We expertize in overseas studying<br/>
-                Get the right tutor you need<br/>
-                Share the knowledge you have
-            </h1>
-            <p>{placeholder}</p>
-          </div>
+          <Col span={20}>
+            <hgroup>
+              <h1>
+                  专注于<span className='font-color-primary'>设计留学</span>辅导<br/>
+                  在这里找到你需要的<span className='font-color-primary'>导师</span><br/>
+                  或成为导师，分享你的<span className='font-color-primary'>知识</span>
+              </h1>
+              <p>留学顾问，就是提供到国外留学等相关咨询服务的人。如，如何选择国外的学校，如何申请国外的学校，如何准备申请材料，如何办理签证，及其相关的一系列信息。顾问，英文为counselor。有某方面的专门知识，供个人或机关团体咨询的人。</p>
+            </hgroup>
+          </Col>
 
-          <div className='Welcome-action'>
-            <Button size='large'>
+          <Col span={4}>
+            <Button size='large' type='primary'>
               <Link to='/orientation' >寻找老师</Link>
             </Button>
 
             <Button size='large'>
               <Link to=''>成为老师</Link>
             </Button>
-          </div>
-        </div>
+          </Col>
+        </Row>
 
         <div className='Welcome-news'>
-          {
-            promote.map((item, index) => {
-              return <section key={index}> <h1>{item}</h1> <p>{placeholder}</p> </section>;
-            })
-          }
         </div>
       </div>
     );
