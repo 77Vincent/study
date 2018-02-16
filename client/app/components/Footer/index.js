ed60dfffef08a6a4adf6baf25f4a5f6a75a8ed80
77Vincent <wentianqi77@outlook.com>
2018-02-16 17:14:16 +0800

    welcome page visual effect

--- client/app/components/Footer/index.js
+++ client/app/components/Footer/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './index.less';
 
 export default class Footer extends React.Component {
   render() {
--- client/app/components/Welcome/index.js
+++ client/app/components/Welcome/index.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'antd';
+import { Button, Carousel } from 'antd';
 import './index.less';
 
 export default class Welcome extends React.Component {
   constructor(props) {
     super(props);
   }
-  
+
+  state = {
+    position: 'center'
+  }
+
+  effect = (e) => {
+    this.setState({
+      position: `-${e.pageX / 2}px -${e.pageY / 2}px`
+    });
+  }
+
   render() {
     return (
-      <div className='Welcome'>
+      <div className='Welcome' onMouseMove={this.effect} >
         <hgroup>
-          <div className='App-background-image' />
+          <div style={{backgroundPosition: `${this.state.position}`}} className='App-background-image' />
 
           <h1>
-              专注于<span className='font-color-primary'>设计</span>辅导<br/>
-              在这找到你需要的<span className='font-color-primary'>导师</span><br/>
-              或成为导师，分享你的<span className='font-color-primary'>知识</span>
+            专注于<span>设计</span>辅导<br/>
+            在这找到你需要的<span>导师</span><br/>
+            或成为导师，分享你的<span>知识</span>
           </h1>
-          {/* <p>留学顾问，就是提供到国外留学等相关咨询服务的人。如，如何选择国外的学校，如何申请国外的学校，如何准备申请材料，如何办理签证，及其相关的一系列信息。顾问，英文为counselor。有某方面的专门知识，供个人或机关团体咨询的人。</p> */}
 
           <Button size='large' type='primary'>
             <Link to='/orientation' >寻找导师</Link>
@@ -29,9 +38,6 @@ export default class Welcome extends React.Component {
             <Link to='/login'>成为导师</Link>
           </Button>
         </hgroup>
-
-        <div className='Welcome-news'>
-        </div>
       </div>
     );
   }
