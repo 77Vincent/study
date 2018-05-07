6b13d5aab6a2d579492896f44017500342b79ad3
77Vincent <wentianqi77@outlook.com>
2018-05-07 21:37:37 +0800

    auth big enhancement

--- routes/sessions/service.js
+++ routes/sessions/service.js
@@ -1,43 +1,14 @@
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
 
 const c = require('../../config')
-const usersService = require('../users/service')
-
-const secret = c.tokenSecret
-const expiresIn = c.cookiesTimeout 
 
 module.exports = {
-  getToken(authorization = '') {
-    if (authorization.split(' ')[0].toLowerCase() === 'bearer') {
-      return authorization.split(' ')[1]
-    }
-    return null
-  },
-  auth: async (id = '', password = '', token = '') => {
-    try {
-      if (token) {
-        const parsed = jwt.verify(token, c.tokenSecret) 
-        id = parsed.id
-        password = parsed.password
-      }
-
-      const user = id ? await usersService.getOneUser(id) : null
-
-      if (user && (bcrypt.compareSync(password, user.password) || password === user.password)) {
-        return user
-      }
-    } catch (err) {
-      throw err
-    }
-  },
   /**
-   * Create token
-   * @param {Object} user 
-   * @return {Object} token and expire time in millisecond 
+   * Sign a jsom web token
+   * @param {object} credentials User Credentials
+   * @return {object} token
    */
-  signToken (user_info) {
-    const token = jwt.sign(user_info, secret, { expiresIn }) 
-    return token
+  signToken (credentials) {
+    return jwt.sign(credentials, c.tokenSecret, { expiresIn: c.cookiesTimeout }) 
   }
 }
--- services/auth.js
+++ services/auth.js
@@ -1,17 +1,35 @@
-const sessionsService = require('../routes/sessions/service')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+const usersService = require('../routes/users/service')
+const c = require('../config')
+
+const getToken = (authorization = '') => {
+  if (authorization.split(' ')[0].toLowerCase() === 'bearer') {
+    return authorization.split(' ')[1]
+  }
+  return null
+}
 
 module.exports = {
   authenticate: async (ctx, next) => {
     try {
-      const { id, password } = ctx.request.body
-      const token = sessionsService.getToken(ctx.request.headers.authorization)
+      let { id, password } = ctx.request.body
+      const token = getToken(ctx.request.headers.authorization)
 
       // Authenticate credentials
-      const user = await sessionsService.auth(id, password, token)    
+      if (token) {
+        const parsed = jwt.verify(token, c.tokenSecret) 
+        id = parsed.id
+        password = parsed.password
+      }
 
-      // Users can only modify their own data
-      // Or sign in as admin
+      const user = id ? await usersService.getOneUser(id) : null
+
       const isValid = user 
+        // Check password 
+        && (bcrypt.compareSync(password, user.password) || password === user.password)
+        // Users can only modify their own data or sign in as admin
         && (String(ctx.params.id) === String(user.dataValues.id) || user.dataValues.role_id === 1)
 
       if (isValid) {
