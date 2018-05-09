88de28f82b62fa62c21a38fd2d7a84787a9c3a03
77Vincent <wentianqi77@outlook.com>
2018-05-09 13:30:28 +0800

    hooks regression

--- routes/hooks.js
+++ routes/hooks.js
@@ -1,40 +1,22 @@
 const Router = require('koa-router')
-const { execSync } = require('child_process')
+const { exec } = require('child_process')
 
 const { General } = require('../services')
 
 const hooks = Router()
-const serverOptions = { encoding: 'utf8' }
-// const webappOptions = Object.assign(serverOptions, {cwd: '../xfolio-webapp'}) 
 
 // This route is for the listening the git webhooks for deployment
 hooks.post('/', (ctx) => {
   try {
 
     // For this server
-    const serverDeployedStart = new Date()
-    execSync('git pull', serverOptions)
-    execSync('npm install', serverOptions)
-    const serverDeployedEnd = new Date()
-
-    // For the webapp
-    // const webappDeployedStart = new Date()
-    // execSync('git pull', webappOptions)
-    // execSync('npm install', webappOptions)
-    // execSync('npm run build', webappOptions)
-    // const webappDeployedEnd = new Date()
+    exec('git pull', (err) => {
+      if (err) { return }
+      exec('npm install')
+    })
 
     ctx.status = 200
-    ctx.body = {
-      deployed_at: {
-        server: serverDeployedEnd,
-        // webapp: webappDeployedEnd 
-      },
-      time_consuming: {
-        server: `${serverDeployedEnd - serverDeployedStart} ms`,
-        // webapp: `${webappDeployedEnd - webappDeployedStart} ms`
-      }
-    } 
+    ctx.body = `Successfully deployed at ${new Date()}` 
   } catch (err) {
     General.logError(ctx, err)
   }
