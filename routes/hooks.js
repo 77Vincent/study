6edab8011c75f5a0c50d83abbfc16fb2d7f69be4
77Vincent <wentianqi77@outlook.com>
2018-05-09 12:41:37 +0800

    do not deploy webapp for now

--- routes/hooks.js
+++ routes/hooks.js
@@ -5,7 +5,7 @@ const { General } = require('../services')
 
 const hooks = Router()
 const serverOptions = { encoding: 'utf8' }
-const webappOptions = Object.assign(serverOptions, {cwd: '../xfolio-webapp'}) 
+// const webappOptions = Object.assign(serverOptions, {cwd: '../xfolio-webapp'}) 
 
 // This route is for the listening the git webhooks for deployment
 hooks.post('/', (ctx) => {
@@ -18,21 +18,21 @@ hooks.post('/', (ctx) => {
     const serverDeployedEnd = new Date()
 
     // For the webapp
-    const webappDeployedStart = new Date()
-    execSync('git pull', webappOptions)
-    execSync('npm install', webappOptions)
-    execSync('npm run build', webappOptions)
-    const webappDeployedEnd = new Date()
+    // const webappDeployedStart = new Date()
+    // execSync('git pull', webappOptions)
+    // execSync('npm install', webappOptions)
+    // execSync('npm run build', webappOptions)
+    // const webappDeployedEnd = new Date()
 
     ctx.status = 200
     ctx.body = {
       deployed_at: {
         server: serverDeployedEnd,
-        webapp: webappDeployedEnd 
+        // webapp: webappDeployedEnd 
       },
       time_consuming: {
         server: `${serverDeployedEnd - serverDeployedStart} ms`,
-        webapp: `${webappDeployedEnd - webappDeployedStart} ms`
+        // webapp: `${webappDeployedEnd - webappDeployedStart} ms`
       }
     } 
   } catch (err) {
