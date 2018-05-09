b35a72da1286326c416fc28fb1844de315232094
77Vincent <wentianqi77@outlook.com>
2018-05-09 12:39:04 +0800

    hooks update

--- routes/hooks.js
+++ routes/hooks.js
@@ -4,19 +4,37 @@ const { execSync } = require('child_process')
 const { General } = require('../services')
 
 const hooks = Router()
+const serverOptions = { encoding: 'utf8' }
+const webappOptions = Object.assign(serverOptions, {cwd: '../xfolio-webapp'}) 
 
 // This route is for the listening the git webhooks for deployment
 hooks.post('/', (ctx) => {
   try {
 
     // For this server
-    execSync('git pull', { encoding: 'utf8' })
+    const serverDeployedStart = new Date()
+    execSync('git pull', serverOptions)
+    execSync('npm install', serverOptions)
+    const serverDeployedEnd = new Date()
 
     // For the webapp
-    execSync('git pull; npm run build', { encoding: 'utf8', cwd: '../xfolio-webapp' })
+    const webappDeployedStart = new Date()
+    execSync('git pull', webappOptions)
+    execSync('npm install', webappOptions)
+    execSync('npm run build', webappOptions)
+    const webappDeployedEnd = new Date()
 
     ctx.status = 200
-    ctx.body = `Successfully deployed at ${new Date()}`
+    ctx.body = {
+      deployed_at: {
+        server: serverDeployedEnd,
+        webapp: webappDeployedEnd 
+      },
+      time_consuming: {
+        server: `${serverDeployedEnd - serverDeployedStart} ms`,
+        webapp: `${webappDeployedEnd - webappDeployedStart} ms`
+      }
+    } 
   } catch (err) {
     General.logError(ctx, err)
   }
