da7591dc888476578d7756a8e97a8bc6851c45f2
77Vincent <wentianqi77@outlook.com>
2018-05-29 06:47:57 +0800

    remove static/doc from tracking

--- static/doc/api_project.js
+++
@@ -1,1 +0,0 @@
-define({  "name": "Xfolio",  "version": "0.0.0",  "description": "Xfolio RESTful API",  "title": "Xfolio RESTful API",  "url": "http://39.104.108.82:3001",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-05-28T22:35:50.951Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
