d0fcadc16980e654e2c3cf6e519a5ad65b5f792a
77Vincent <wentianqi77@outlook.com>
2018-06-03 14:26:48 +0800

    sessions doc update

--- routes/sessions/index.js
+++ routes/sessions/index.js
@@ -10,7 +10,7 @@ const sessions = Router()
 /** 
  * @api {post} /api/sessions Sign in
  * @apiGroup Sessions
- * @apiParam {String} id User ID
+ * @apiParam {String} id User ID (id, username, mobilephone, email)
  * @apiParam {String} password User password 
  * @apiSuccess (200) {Object} void Object containing user object and token
  * @apiSuccess (401) {Void} void Authentication failed
